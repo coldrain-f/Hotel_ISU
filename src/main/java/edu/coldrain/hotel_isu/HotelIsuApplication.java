9ce4cec32e1708fa1d873dfba1307da49828ec1c
coldrain-f <intmain__@naver.com>
2021-09-17 18:27:49 +0900

    로그 수정

--- src/main/java/edu/coldrain/hotel_isu/HotelIsuApplication.java
+++ src/main/java/edu/coldrain/hotel_isu/HotelIsuApplication.java
@@ -9,5 +9,4 @@ public class HotelIsuApplication {
     public static void main(String[] args) {
         SpringApplication.run(HotelIsuApplication.class, args);
     }
-
 }
---
+++ src/main/java/edu/coldrain/hotel_isu/controller/user/HomeController.java
@@ -0,0 +1,14 @@
+package edu.coldrain.hotel_isu.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+
+        return "redirect:/home/login";
+    }
+}
